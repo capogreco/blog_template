cb682f715c463f9795107fbca9b9f5cee64d78a3
capo_greco <thomas.capogreco@gmail.com>
2025-03-31 11:19:08 +1100

    fixing for .ts in posts.ts

--- routes/index.tsx
+++ routes/index.tsx
@@ -14,7 +14,7 @@ export default function BlogIndexPage(props: PageProps<Post[]>) {
     <main class="max-w-screen-md px-4 pt-16 mx-auto">
       <h1 class="text-5xl font-bold">Blog</h1>
       <div class="mt-8">
-        {posts.map((post) => <PostCard post={post} />)}
+        {posts.map(post => <PostCard post={post} />)}
       </div>
     </main>
   );
--- utils/posts.ts
+++ utils/posts.ts
@@ -29,8 +29,19 @@ export async function getPosts(): Promise<Post[]> {
 
 // Get post.
 export async function getPost(slug: string): Promise<Post | null> {
-  const text = await Deno.readTextFile(join(DIRECTORY, `${slug}.md`));
-  const { attrs, body } = extract(text);
+  const filePath = join(DIRECTORY, `${slug}.md`);
+  const text = await Deno.readTextFile(filePath);
+  const { attrs, body } = extract(text) as {
+    attrs: {
+      title: string;
+      published_at: string;
+      snippet: string;
+      disable_html_sanitization: boolean;
+      allow_math: boolean;
+    };
+    body: string;
+  };
+
   return {
     slug,
     title: attrs.title,
